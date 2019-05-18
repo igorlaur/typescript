d940ee0a7fffbae02f5c0251cd705acadf5dbcba
Igor Laur <igor.laur2@gmail.com>
2019-05-18 20:43:13 -0300

    Lodash

--- dist/app.js
+++ dist/app.js
@@ -2,6 +2,8 @@
 exports.__esModule = true;
 var base_ships_1 = require("./base-ships");
 var starfihter_1 = require("./starfihter");
+var _ = require("lodash");
+console.log(_.pad("Typescript Examples", 40, "="));
 var ship = new base_ships_1.Spacecraft('hyperdrive');
 ship.jumpIntoHyperspace();
 var falcon = new starfihter_1.MillenniumFalcon();
